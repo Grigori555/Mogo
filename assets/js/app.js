f654906db6836a8bdd378be8d48766b7b477641d
grisha <morozehs@yandex.ru>
2020-07-11 22:29:12 +0300

    fixed menu scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,29 @@ $(function() {
 
     var header = $("#header"),
      introH =$("#intro").innerHeight(), // сохраняем высоту id intro
-    scrollOffset = 0;
+    scrollOffset = $(window).scrollTop();
+
+    checkScroll(scrollOffset);
     
     $(window).on("scroll",function(){
 
         scrollOffset = $(this).scrollTop();
-            if( scrollOffset >= introH ){
-                header.addClass("fixed");
 
-            } else{
-                header.removeClass("fixed");
-            }
+        checkScroll(scrollOffset);
     
     });
+
+
+
+    function checkScroll(scrollOffset){
+        
+        if( scrollOffset >= introH ){
+            header.addClass("fixed");
+
+        } else{
+            header.removeClass("fixed");
+        }
+    }
 });
+
+
