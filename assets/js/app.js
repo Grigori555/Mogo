21d3d7e4930de89411a5cfdd19af8ee614b28d9c
grisha <morozehs@yandex.ru>
2020-07-11 14:17:52 +0300

    added class fixed use jq

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,20 @@
 /*Work with jquery*/
+
+$(function() {
+
+    var header = $("#header"),
+     introH =$("#intro").innerHeight(), // сохраняем высоту id intro
+    scrollOffset = 0;
+    
+    $(window).on("scroll",function(){
+
+        scrollOffset = $(this).scrollTop();
+            if( scrollOffset >= introH ){
+                header.addClass("fixed");
+
+            } else{
+                header.removeClass("fixed");
+            }
+    
+    });
+});
