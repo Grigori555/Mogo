7421f58f5f50b7acccab08bb7a832bc303bea5e4
grisha <morozehs@yandex.ru>
2020-07-15 19:34:29 +0300

    Collapse accordion

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,14 +49,29 @@ $(function() {
 
     });
 
-});
-
-/* Menu nav toggle  */
+    /* Menu nav toggle  */
 $("#nav_toggle").on("click", function(event){
     event.preventDefault();
     $(this).toggleClass("active");
     $("#nav").toggleClass("active");
+    });
+
+/* Collapse */
+$("[data-collapse]").on("click",function(event){
+    event.preventDefault();
+    var $this =$(this),
+        blockId =$this.data("collapse");
+    $this.toggleClass("active");
+    
+
 });
+
+});
+
+
+
+
+
 
 
 
